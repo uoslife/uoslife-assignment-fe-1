bdcb2fd6f6e81ca85c9343603c9818448a4b535e
John Doeexit <2002eojin@gmail.com>
2023-07-02 16:54:07 +0900

    feat: line-height 적용

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,3 +1,16 @@
+import { styled } from "styled-components";
+
+const Container = styled.div<{
+  fontSize: number;
+  fontWeight: number;
+  color: string;
+}>`
+  font-size: ${({ fontSize }) => fontSize}px;
+  line-height: ${({ fontSize }) => fontSize * 1.5}px;
+  font-weight: ${({ fontWeight }) => fontWeight};
+  color: ${({ color }) => color};
+`;
+
 const Text = ({
   text,
   fontSize,
@@ -9,7 +22,11 @@ const Text = ({
   fontWeight: number;
   color: string;
 }) => {
-  return <></>;
+  return (
+    <Container fontSize={fontSize} fontWeight={fontWeight} color={color}>
+      {text}
+    </Container>
+  );
 };
 
 export default Text;
