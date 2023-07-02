9de519bba787b37135d663e64b6176d3305a43b7
John Doeexit <2002eojin@gmail.com>
2023-07-02 17:02:41 +0900

    feat: 범용 type 및 styled-components 세팅, 내용물 작성

--- src/components/Cafeteria/Cafeteria.tsx
+++ src/components/Cafeteria/Cafeteria.tsx
@@ -1,7 +1,127 @@
+import { styled } from "styled-components";
 import { CafeteriaType } from "./Cafeteria.type";
+import Text from "../Text/Text";
+
+const Wrapper = styled.div`
+  margin-bottom: 16px;
+  border-radius: 16px;
+  border: 0.666667px solid rgb(222, 225, 229);
+`;
+
+type P = {
+  paddingLeft?: number;
+  paddingRight?: number;
+  paddingTop?: number;
+  paddingBottom?: number;
+};
+
+type M = {
+  marginLeft?: number;
+  marginRight?: number;
+  marginTop?: number;
+  marginBottom?: number;
+};
+
+const Row = styled.div<P & M>`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+
+  padding-top: ${(({ paddingTop }) => paddingTop) || 0.1}px;
+  padding-bottom: ${(({ paddingBottom }) => paddingBottom) || 0.1}px;
+  padding-left: ${(({ paddingLeft }) => paddingLeft) || 0.1}px;
+  padding-right: ${(({ paddingRight }) => paddingRight) || 0.1}px;
+
+  margin-top: ${(({ marginTop }) => marginTop) || 0.1}px;
+  margin-bottom: ${(({ marginBottom }) => marginBottom) || 0.1}px;
+  margin-left: ${(({ marginLeft }) => marginLeft) || 0.1}px;
+  margin-right: ${(({ marginRight }) => marginRight) || 0.1}px;
+`;
+
+const Col = styled.div<P & M>`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  padding-top: ${(({ paddingTop }) => paddingTop) || 0.1}px;
+  padding-bottom: ${(({ paddingBottom }) => paddingBottom) || 0.1}px;
+  padding-left: ${(({ paddingLeft }) => paddingLeft) || 0.1}px;
+  padding-right: ${(({ paddingRight }) => paddingRight) || 0.1}px;
+
+  margin-top: ${(({ marginTop }) => marginTop) || 0.1}px;
+  margin-bottom: ${(({ marginBottom }) => marginBottom) || 0.1}px;
+  margin-left: ${(({ marginLeft }) => marginLeft) || 0.1}px;
+  margin-right: ${(({ marginRight }) => marginRight) || 0.1}px;
+`;
 
 const Cafeteria = ({ cafeteria }: { cafeteria: CafeteriaType }) => {
-  return <></>;
+  return (
+    <Wrapper>
+      <Row
+        paddingTop={12}
+        paddingBottom={12}
+        paddingLeft={16}
+        paddingRight={16}>
+        <Text
+          text={cafeteria.location}
+          fontSize={18}
+          fontWeight={600}
+          color={"black"}
+        />
+        <Text
+          text={"자세히보기"}
+          fontSize={15}
+          fontWeight={500}
+          color="rgb(58, 136, 245)"
+        />
+      </Row>
+      <Col
+        paddingTop={10}
+        paddingBottom={16}
+        paddingLeft={16}
+        paddingRight={16}>
+        <Row marginBottom={8}>
+          <Text
+            text={cafeteria.mainMenu}
+            fontSize={18}
+            fontWeight={600}
+            color={"black"}
+          />
+          <Text
+            text={cafeteria.openingHours}
+            color={"rgb(128, 138, 152)"}
+            fontSize={15}
+            fontWeight={500}
+          />
+        </Row>
+        <Row>
+          <Text
+            text={cafeteria.detailMenu}
+            color={"rgb(128, 138, 152)"}
+            fontSize={15}
+            fontWeight={500}
+          />
+        </Row>
+        <Row>
+          <Text
+            text={cafeteria.nutritionalInfo}
+            color={"rgb(151, 161, 174)"}
+            fontSize={11}
+            fontWeight={500}
+          />
+          <Text
+            text={cafeteria.price}
+            color={"rgb(59, 64, 70)"}
+            fontSize={18}
+            fontWeight={600}
+          />
+        </Row>
+      </Col>
+    </Wrapper>
+  );
 };
 
 export default Cafeteria;
