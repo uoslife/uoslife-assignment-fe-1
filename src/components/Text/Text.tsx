b92be13962b41a9fc2279f12b7bb41d49f090a6b
youngchan Kim <aacz12035@gmail.com>
2023-07-03 15:41:21 +0900

    feat: Text component 제작

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,5 +1,24 @@
-const Text = () => {
-	return <></>;
+import { styled } from "styled-components";
+
+const StyledText = styled.div<textProps>`
+  font-weight: ${({ weight }) => weight};
+  font-size: ${({ size }) => size}px;
+  color: ${({ color }) => color};
+`;
+
+export type textProps = {
+  text: string;
+  size: number;
+  weight: number;
+  color: string;
+};
+
+const Text = ({ text, size, weight, color }: textProps) => {
+  return (
+    <StyledText text={text} size={size} weight={weight} color={color}>
+      {text}
+    </StyledText>
+  );
 };
 
 export default Text;
