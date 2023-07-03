9ca49bf8557fd031bb789ac45a1f076d86cc0d69
youngchan Kim <aacz12035@gmail.com>
2023-07-03 16:34:34 +0900

    fix: Text 컴포넌트 size props 관련 리팩터링

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,22 +1,48 @@
-import { styled } from "styled-components";
+import { theme } from "../../GlobalStyle";
+import styled, { css } from "styled-components";
 
 const StyledText = styled.div<textProps>`
   font-weight: ${({ weight }) => weight};
-  font-size: ${({ size }) => size}px;
   color: ${({ color }) => color};
+  ${({ size }) => getTextSize(size)};
 `;
 
 export type textProps = {
-  text: string;
-  size: number;
-  weight: number;
-  color: string;
+  label?: string;
+  size?: "sm" | "md" | "lg";
+  weight?: number;
+  color?: string;
 };
 
-const Text = ({ text, size, weight, color }: textProps) => {
+const getTextSize = (size?: textProps["size"]) => {
+  switch (size) {
+    case "sm": {
+      return css`
+        ${theme.Text_sm};
+      `;
+    }
+    case "md": {
+      return css`
+        ${theme.Text_md};
+      `;
+    }
+    case "lg": {
+      return css`
+        ${theme.Text_lg};
+      `;
+    }
+    default: {
+      return css`
+        ${theme.Text_md};
+      `;
+    }
+  }
+};
+
+const Text = ({ label, size, weight = 500, color }: textProps) => {
   return (
-    <StyledText text={text} size={size} weight={weight} color={color}>
-      {text}
+    <StyledText label={label} size={size} weight={weight} color={color}>
+      {label}
     </StyledText>
   );
 };
