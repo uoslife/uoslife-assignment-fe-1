287ad9e7cc9a724d7cf842290d97ed36c96accb9
youngchan Kim <aacz12035@gmail.com>
2023-07-03 16:33:45 +0900

    feat: color, typography config 추가

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,5 +1,12 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { colors } from "./utils/styles/color.config";
+import { typographies } from "./utils/styles/typography.config";
+
+export const theme = {
+  ...colors,
+  ...typographies,
+};
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
