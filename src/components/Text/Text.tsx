cf72a16fc435c7f84162a162d3a3cfe029ffe419
alicehrk <alicehrk23@gmail.com>
2023-10-14 21:06:17 +0900

    feat: add Text component

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,5 +1,28 @@
-const Text = () => {
-	return <></>;
+import { FC } from "react";
+import styled from "styled-components";
+
+interface StyledTextProps {
+  fontSize?: string;
+  fontWeight?: string;
+  color?: string;
+}
+
+const StyledText = styled.div<StyledTextProps>`
+  font-size: ${(props) => props.fontSize}px;
+  font-weight: ${(props) => props.fontWeight};
+  color: ${(props) => props.color};
+`;
+
+interface TextProps extends StyledTextProps {
+  text: string;
+}
+
+const Text: FC<TextProps> = ({ text, fontSize, fontWeight, color }) => {
+  return (
+    <StyledText fontSize={fontSize} fontWeight={fontWeight} color={color}>
+      {text}
+    </StyledText>
+  );
 };
 
 export default Text;
